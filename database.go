08a19c08bcfbe21e4f1352ee6179808fcd647198
jldunsmore <jldunsmore@proton.me>
2026-02-11 18:55:12 -0600

    messing about with json data

--- database.go
+++ database.go
@@ -1,23 +1,171 @@
 package main
 
-type Book struct {
-	Title string
-	Desc  string
-	Type  string
-	ISBN  string
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+type Title struct {
+	Title string `json:"title"`
+	Id    string `json:"id"`
 }
 
-var bookDatabase = map[string]Book{
-	"9781386123613": {
-		Title: "The Two Week Curse",
-		Desc:  "Two soldiers are transported to a world of magic, cultivation and stat screens and have to rely on old skills and newly learned ones to survive.",
-		Type:  "E-Book",
-		ISBN:  "9781386123613",
-	},
-	"9781638493495": {
-		Title: "The Two Week Curse",
-		Desc:  "Two soldiers are transported to a world of magic, cultivation and stat screens and have to rely on old skills and newly learned ones to survive.",
-		Type:  "Hardcover",
-		ISBN:  "9781638493495",
-	},
+type BookAuthors struct {
+	Authors []string `json:"authors"`
+	ISBN    string   `json:"isbn"`
+}
+
+type Author struct {
+	Name string
+	ISBN []string
+}
+
+type Book struct {
+	Kind       string `json:"kind"`
+	ID         string `json:"id"`
+	Etag       string `json:"etag"`
+	SelfLink   string `json:"selfLink"`
+	VolumeInfo struct {
+		Title               string   `json:"title"`
+		Authors             []string `json:"authors"`
+		PublishedDate       string   `json:"publishedDate"`
+		Description         string   `json:"description"`
+		IndustryIdentifiers []struct {
+			Type       string `json:"type"`
+			Identifier string `json:"identifier"`
+		} `json:"industryIdentifiers"`
+		ReadingModes struct {
+			Text  bool `json:"text"`
+			Image bool `json:"image"`
+		} `json:"readingModes"`
+		PageCount           int    `json:"pageCount"`
+		PrintType           string `json:"printType"`
+		MaturityRating      string `json:"maturityRating"`
+		AllowAnonLogging    bool   `json:"allowAnonLogging"`
+		ContentVersion      string `json:"contentVersion"`
+		PanelizationSummary struct {
+			ContainsEpubBubbles  bool `json:"containsEpubBubbles"`
+			ContainsImageBubbles bool `json:"containsImageBubbles"`
+		} `json:"panelizationSummary"`
+		ImageLinks struct {
+			SmallThumbnail string `json:"smallThumbnail"`
+			Thumbnail      string `json:"thumbnail"`
+		} `json:"imageLinks"`
+		Language            string `json:"language"`
+		PreviewLink         string `json:"previewLink"`
+		InfoLink            string `json:"infoLink"`
+		CanonicalVolumeLink string `json:"canonicalVolumeLink"`
+	} `json:"volumeInfo"`
+	SaleInfo struct {
+		Country     string `json:"country"`
+		Saleability string `json:"saleability"`
+		IsEbook     bool   `json:"isEbook"`
+	} `json:"saleInfo"`
+	AccessInfo struct {
+		Country                string `json:"country"`
+		Viewability            string `json:"viewability"`
+		Embeddable             bool   `json:"embeddable"`
+		PublicDomain           bool   `json:"publicDomain"`
+		TextToSpeechPermission string `json:"textToSpeechPermission"`
+		Epub                   struct {
+			IsAvailable bool `json:"isAvailable"`
+		} `json:"epub"`
+		Pdf struct {
+			IsAvailable bool `json:"isAvailable"`
+		} `json:"pdf"`
+		WebReaderLink       string `json:"webReaderLink"`
+		AccessViewStatus    string `json:"accessViewStatus"`
+		QuoteSharingAllowed bool   `json:"quoteSharingAllowed"`
+	} `json:"accessInfo"`
+	SearchInfo struct {
+		TextSnippet string `json:"textSnippet"`
+	} `json:"searchInfo"`
+}
+
+type Books struct {
+	Books []Book `json:"items"`
+}
+
+// func GetBookByISBN(ISBN string) {
+// }
+
+// func GetBooksByTitle(Title string) {
+// }
+
+// func GetBooksByAuthor(author string) {
+// }
+
+func GetListByAuthor() []Author {
+	var books = database()
+
+	return getListByAuthor(books)
+}
+
+func database() Books {
+	bookJsonFile, err := os.Open("books.json")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Successfully opened books.json")
+
+	defer bookJsonFile.Close()
+
+	byteValue, _ := io.ReadAll(bookJsonFile)
+
+	var books Books
+
+	json.Unmarshal(byteValue, &books)
+
+	return books
+	// if searchType == "ISBN" {
+	// 	searchByISBN(books, value)
+	// } else if searchType == "Title" {
+	// 	searchByTitle(books, value)
+	// } else if searchType == "Author" {
+	// 	return searchByAuthor(books, value)
+	// }
+	// return empty
+}
+
+// func searchByISBN(books Books, isbn string) {
+// 	fmt.Printf("Searching for ISBN: %s\n", isbn)
+// }
+
+// func searchByTitle(books Books, title string) {
+// 	fmt.Printf("Searching for title: %s\n", title)
+// }
+
+func getListByAuthor(books Books) []Author {
+
+	var authors []Author
+	for i := 0; i < len(books.Books); i++ {
+		authorList := BookAuthors{
+			Authors: books.Books[i].VolumeInfo.Authors,
+			ISBN:    books.Books[i].VolumeInfo.IndustryIdentifiers[0].Identifier,
+		}
+		for j := 0; j < len(authorList.Authors); j++ {
+			authorname := authorList.Authors[j]
+			authorExists := false
+			for k := 0; k < len(authors); k++ {
+				if authors[k].Name == authorname {
+					authors[k].ISBN = append(authors[k].ISBN, authorList.ISBN)
+					authorExists = true
+					break
+				}
+			}
+			if !authorExists {
+				newAuthor := Author{
+					Name: authorname,
+					ISBN: []string{authorList.ISBN},
+				}
+				authors = append(authors, newAuthor)
+			}
+		}
+	}
+	//fmt.Printf("Authors: %+v\n", authors)
+	return authors
 }
--- handlers.go
+++ handlers.go
@@ -10,72 +10,118 @@ import (
 func handleClientProfile(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		GetBook(w, r)
+		SearchDatabase(w, r)
 	case http.MethodPatch:
-		UpdateBook(w, r)
+		//UpdateBook(w, r)
+		fmt.Fprintln(w, "Update book endpoint is under construction.")
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
 }
 
-func GetBook(w http.ResponseWriter, r *http.Request) {
+func SearchDatabase(w http.ResponseWriter, r *http.Request) {
 	user := r.Context().Value("user").(User)
-	log.Println("Looking for book...", user.Name)
+	log.Println("searching database, ", user.Name)
 
-	var isbn = r.URL.Query().Get("isbn")
-	book, ok := bookDatabase[isbn]
-	if !ok || isbn == "" {
-		http.Error(w, "Book not found", http.StatusNotFound)
+	var searchTerm = r.URL.Query().Get("search")
+	if searchTerm == "" {
+		http.Error(w, "search query parameter is required", http.StatusBadRequest)
 		return
+	} else {
+		fmt.Printf("Search term: %s\n", searchTerm)
+		switch searchTerm {
+		case "BookByISBN":
+			//GetBookByISBN(w, r)
+		case "BooksByTitle":
+			//GetBooksByTitle(w, r)
+		case "BooksByAuthor":
+			//GetBookByAuthor(w, r)
+		case "ListOfBooksByAuthor":
+			GetListOfBookByAuthor(w, r)
+		default:
+			http.Error(w, "Invalid search type", http.StatusBadRequest)
+		}
 	}
+}
 
-	response := Book{
-		Title: book.Title,
-		Desc:  book.Desc,
-		Type:  book.Type,
-		ISBN:  book.ISBN,
-	}
+func GetBookByISBN(w http.ResponseWriter, r *http.Request) {
+	user := r.Context().Value("user").(User)
+	log.Println("Looking for book, ", user.Name)
+
+	//var isbn = r.URL.Query().Get("isbn")
+	//response = database("ISBN", isbn)
+
+	//w.Header().Set("Content-Type", "application/json")
+
+	//json.NewEncoder(w).Encode(response)
+}
+
+func GetBookByTitle(w http.ResponseWriter, r *http.Request) {
+	user := r.Context().Value("user").(User)
+	log.Println("Looking for book, ", user.Name)
+
+	//var title = r.URL.Query().Get("title")
+	//database("Title", title)
+}
+
+func GetListOfBookByAuthor(w http.ResponseWriter, r *http.Request) {
+	user := r.Context().Value("user").(User)
+	log.Println("Making list, ", user.Name)
+
+	var response = GetListByAuthor()
 
 	w.Header().Set("Content-Type", "application/json")
 
 	json.NewEncoder(w).Encode(response)
 }
 
-func UpdateBook(w http.ResponseWriter, r *http.Request) {
-	user := r.Context().Value("user").(User)
-	log.Println("Updating book...", user.Name)
+// func GetBook(w http.ResponseWriter, r *http.Request) {
+// 	user := r.Context().Value("user").(User)
+// 	log.Println("Looking for book...", user.Name)
 
-	var isbn = r.URL.Query().Get("isbn")
-	book, ok := bookDatabase[isbn]
-	if !ok || isbn == "" {
-		http.Error(w, "Book not found", http.StatusNotFound)
-		return
-	}
+// 	var isbn = r.URL.Query().Get("isbn")
+// 	database("ISBN", isbn)
 
-	// Decode the JSON payload directly into struct
-	var payloadData Book
-	if err := json.NewDecoder(r.Body).Decode(&payloadData); err != nil {
-		http.Error(w, "Invalid JSON", http.StatusBadRequest)
-		return
-	}
-	defer r.Body.Close()
+// 	w.Header().Set("Content-Type", "application/json")
 
-	// Update Book
-	if payloadData.Title != "" {
-		book.Title = payloadData.Title
-	}
-	if payloadData.Desc != "" {
-		book.Desc = payloadData.Desc
-	}
-	if payloadData.Type != "" {
-		book.Type = payloadData.Type
-	}
-	if payloadData.ISBN != "" {
-		book.ISBN = isbn
-	}
-	bookDatabase[book.ISBN] = book
+// 	json.NewEncoder(w).Encode(response)
+// }
 
-	fmt.Printf("Book updated: %+v\n", book)
+// func UpdateBook(w http.ResponseWriter, r *http.Request) {
+// 	user := r.Context().Value("user").(User)
+// 	log.Println("Updating book...", user.Name)
 
-	w.WriteHeader(http.StatusOK)
-}
+// 	var isbn = r.URL.Query().Get("isbn")
+// 	book, ok := bookDatabase[isbn]
+// 	if !ok || isbn == "" {
+// 		http.Error(w, "Book not found", http.StatusNotFound)
+// 		return
+// 	}
+
+// 	// Decode the JSON payload directly into struct
+// 	var payloadData Book
+// 	if err := json.NewDecoder(r.Body).Decode(&payloadData); err != nil {
+// 		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+// 		return
+// 	}
+// 	defer r.Body.Close()
+
+// 	// Update Book
+// 	if payloadData.Title != "" {
+// 		book.Title = payloadData.Title
+// 	}
+// 	if payloadData.Desc != "" {
+// 		book.Desc = payloadData.Desc
+// 	}
+// 	if payloadData.Type != "" {
+// 		book.Type = payloadData.Type
+// 	}
+// 	if payloadData.ISBN != "" {
+// 		book.ISBN = isbn
+// 	}
+// 	bookDatabase[book.ISBN] = book
+
+// 	fmt.Printf("Book updated: %+v\n", book)
+
+// 	w.WriteHeader(http.StatusOK)
+// }
